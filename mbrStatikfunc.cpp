931e701c5d9d831eeedc5c108dc448b8d4020ce9
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-05-17 15:09:43 +0700

    deklarasi kelas

---
+++ mbrStatikfunc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+    static int nim;  // Static member variable
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; }  // Static member function
+    static int getNim() { return nim; }           // Static member function
+
+    mahasiswa(string pnama) : nama(pnama) { setID(); }
+};
+
