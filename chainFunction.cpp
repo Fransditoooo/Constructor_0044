92304c53244ee9156e99181beca6e163d9408db7
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-05-17 14:28:17 +0700

    implementasi

--- chainFunction.cpp
+++ chainFunction.cpp
@@ -7,4 +7,13 @@ class buku
 
 public:
     buku setJudul(string judul)
-  
+    {
+        this->judul = judul;
+        return *this; // chain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
+
