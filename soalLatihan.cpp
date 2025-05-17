c543c5c2edd1805077e2b327bf389a1a1b2d25bd
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-05-17 15:52:47 +0700

    deklarasi kelas peminjam

--- soalLatihan.cpp
+++ soalLatihan.cpp
@@ -23,3 +23,20 @@ public:
     friend void lihatStatistik(Admin a, Buku b);
 };
 
+class Peminjam {
+private:
+    string nama;
+    int ID;
+    int totalPinjaman;
+
+public:
+    Peminjam(string n, int id) : nama(n), ID(id), totalPinjaman(0) {}
+
+    void tampilkanInfo() {
+        cout << "Nama: " << nama << ", ID: " << ID << ", Total Pinjaman: " << totalPinjaman << endl;
+    }
+
+    friend class Petugas;
+    friend void lihatStatistik(Admin a, Peminjam p);
+};
+
