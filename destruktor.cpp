e2e708af1c7bac44fb33114a6c5ef6f1c57acf21
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-05-17 15:50:05 +0700

    deklarasi kelas

--- destruktor.cpp
+++ destruktor.cpp
@@ -16,7 +16,7 @@ public:
 angka::angka(int i) {  // Constructor
     panjang = i;
     arr = new int[i];
-    isiData();
+    isiData(); 
 }
 
 angka::~angka() {  // Destructor
---
+++ soalLatihan.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Petugas;
+class Admin;
+
+class Buku {
+private:
+    string judul;
+    string penulis;
+    bool dipinjam;
+
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    void tampilkanInfo() {
+        cout << "Judul: " << judul << ", Penulis: " << penulis << ", Status: " 
+             << (dipinjam ? "Dipinjam" : "Tersedia") << endl;
+    }
+
+    friend class Petugas;
+    friend void lihatStatistik(Admin a, Buku b);
+};
+
